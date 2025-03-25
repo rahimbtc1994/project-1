cf233925444e47a2fea56ea5d21dde21b5a423ae
rahimbtc1994 <rahimbtc1994@gmail.com>
2025-03-25 22:35:04 +0100

    project-1: add repo, continue

--- patient-management/patient-service/src/main/java/com/pm/patientservice/repository/PatientRepository.java
+++ patient-management/patient-service/src/main/java/com/pm/patientservice/repository/PatientRepository.java
@@ -1,4 +1,12 @@
 package com.pm.patientservice.repository;
 
-public interface PatientRepository {
+import com.pm.patientservice.model.Patient;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PatientRepository extends JpaRepository<Patient, UUID> {
+    boolean existsByEmail(String email);
+    boolean existsByEmailAndIdNot(String email, UUID id);
 }
